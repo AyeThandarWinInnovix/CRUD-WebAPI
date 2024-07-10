57e5197eb5c65113e085d8ba315ffae2810c90ba
aye <ayethandarwin@innovix-solutions.com>
2024-07-10 09:48:40 +0630

    modify user password format with regular expression

--- WebApi/Dtos/UserDtos/UserDto.cs
+++ WebApi/Dtos/UserDtos/UserDto.cs
@@ -10,6 +10,8 @@ namespace WebApi.Dtos.UserDtos
         public string Username { get; set; } = null!;
 
         [StringLength(255)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$",
+        ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
         public string Password { get; set; } = null!;
 
         [EmailAddress]
